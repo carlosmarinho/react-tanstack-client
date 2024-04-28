003077eff3beef3491370a63d1aad7e9fddf3168
Carlos Marinho <carlos.marinho@energage.com>
2024-04-28 18:29:17 -0300

    add a new menu and make menu with same typography for consistency

--- src/components/layouts/header/MiddleNav.tsx
+++ src/components/layouts/header/MiddleNav.tsx
@@ -1,4 +1,4 @@
-import { styled } from "@mui/material";
+import { Typography, styled } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { Nav } from "./LeftNav";
 
@@ -8,10 +8,17 @@ const NavLinkCustom = styled(NavLink)({
 });
 
 function MiddleNav() {
+  const handleLoadFakeClient = () => {};
+
   return (
     <Nav>
-      <NavLinkCustom to="/list-client">Listar Clientes</NavLinkCustom>
-      <NavLinkCustom to="/add-client">Adicionar Cliente</NavLinkCustom>
+      <NavLinkCustom to="/list-client">
+        <Typography>Listar Clientes</Typography>
+      </NavLinkCustom>
+      <NavLinkCustom to="/add-client">
+        <Typography>Adicionar Cliente</Typography>
+      </NavLinkCustom>
+      <Typography onClick={handleLoadFakeClient}>Load fake Clientes</Typography>
     </Nav>
   );
 }
