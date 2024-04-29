c056579a8c51738af78aba73ccf561754fd280f8
Carlos Marinho <carlos.marinho@energage.com>
2024-04-28 22:25:14 -0300

    Add index.tsx to maintain file structure pattern

---
+++ src/components/clients/list/index.tsx
@@ -0,0 +1,1 @@
+export { default as ListClient } from "./ListClient";
--- src/pages/client/List.tsx
+++ src/pages/client/List.tsx
@@ -1,4 +1,4 @@
-import ListClient from "../../components/clients/list/ListClient";
+import { ListClient } from "../../components/clients/list";
 import { Layout } from "../../components/layouts";
 
 function ListClientPage() {
