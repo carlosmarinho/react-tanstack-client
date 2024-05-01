3e818dd0102f6e80c27f0581353c8faff5694595
Carlos Marinho <carlos.marinho@energage.com>
2024-05-01 12:57:51 -0300

    create test for ClientListItem

---
+++ src/components/clients/list/test/ClientListItem.test.tsx
@@ -0,0 +1,78 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ClientListItem from "../ClientListItem";
+import { mockClients } from "../../../../mocks/clients";
+import { MemoryRouter } from "react-router-dom";
+
+const clientPF = mockClients[0];
+
+// Mock the Link component
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  Link: jest.fn(({ to, children }) => (
+    <div data-testid="link" data-to={to}>
+      {children}
+    </div>
+  )),
+}));
+
+describe("ClientListItem", () => {
+  // Component renders properly
+  it("renders properly", () => {
+    const openDeleteModalMock = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <ClientListItem
+          client={clientPF}
+          openDeleteModal={openDeleteModalMock}
+        />
+      </MemoryRouter>
+    );
+
+    // I need to check the 'nome' cause it belongs to a client PF, else I get a typescript error on clientPF.nome
+    if ("nome" in clientPF) {
+      const clientName = screen.getByText(clientPF.nome);
+      expect(clientName).toBeInTheDocument();
+    }
+  });
+
+  // Clicking on edit button
+  it("navigates to the correct path when edit button is clicked", async () => {
+    const openDeleteModalMock = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <ClientListItem
+          client={clientPF}
+          openDeleteModal={openDeleteModalMock}
+        />
+      </MemoryRouter>
+    );
+
+    const editButton = screen.getByLabelText(/Edit client/i); // replace with the actual aria-label of your edit button
+    await userEvent.click(editButton);
+
+    const link = screen.getByTestId("link");
+    expect(link).toHaveAttribute("data-to", `/edit-client/${clientPF.id}`);
+  });
+
+  //Clicking on delete button
+  it("calls openDeleteModal when delete button is clicked", async () => {
+    const openDeleteModalMock = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <ClientListItem
+          openDeleteModal={openDeleteModalMock}
+          client={clientPF}
+        />
+      </MemoryRouter>
+    );
+
+    const deleteButton = screen.getByLabelText(`Delete client ${clientPF.id}`);
+    await userEvent.click(deleteButton);
+
+    expect(openDeleteModalMock).toHaveBeenCalled();
+  });
+});
